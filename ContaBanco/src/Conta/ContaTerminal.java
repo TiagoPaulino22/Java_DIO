9d223934e6aa6cad223044e97050952450300648
Tiago22 <tiagopaulinotps@gmail.com>
2025-03-19 19:13:01 -0300

    projeto conta de banco

---
+++ ContaBanco/src/Conta/ContaTerminal.java
@@ -0,0 +1,43 @@
+package Conta;
+
+import java.util.Scanner; 
+
+public class ContaTerminal {
+
+	public static void main(String[] args) {
+		
+		// Atributos 
+		 
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		
+		System.out.println("Olá! Vamos criar uma conta? \n");
+		System.out.println("Aperte Enter para continuar. \n");
+		String continua = entrada.nextLine();
+		
+			
+		System.out.println("Por favor, digite o seu nome.");
+		String nomeCliente = entrada.nextLine();
+		
+		
+		System.out.println("Por favor, digite o número da Agência com 4 dígitos.");
+		String agencia = entrada.next();
+		StringBuilder agenciaFormatada = new StringBuilder(agencia); 
+		
+		
+		
+		System.out.println("Por favor, digite o número da conta com 4 dígitos.");
+		int numero = entrada.nextInt();
+		
+		System.out.println("Você precisa depositar um valor inicial para abrir a conta. Digite o valor que deseja depositar.");
+		double saldo = entrada.nextDouble();
+		
+		
+		System.out.println("Olá," + nomeCliente.toUpperCase() + ". " + "Obrigado por criar uma conta em nosso banco, sua agência é " + agenciaFormatada.insert(3, "-") + ", "+ "conta " + numero + " e seu saldo " + "R$"+ saldo + " já está disponível para saque.");
+ 
+		
+	
+	}
+
+}
